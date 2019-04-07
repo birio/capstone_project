dc29b75fe0b043f0ea0671d0ed5c38d26e89d80a
Alberto Guasco <albgua01@e112592-vm1.nice.arm.com>
2019-04-07 22:28:49 +0200

    add random_stimuli algo

---
+++ print_stats.py
@@ -0,0 +1,51 @@
+
+def clean_str_config(str_config):
+   str_config = str_config.replace("[","")
+   str_config = str_config.replace("]","")
+   str_config = str_config.replace(" ","")
+   str_config = str_config.replace(",","_")
+   return str_config
+
+
+def print_episode(i_episode, reward, n_comb, n_states, str_config):
+   if i_episode%10==0:
+      print("episode ", i_episode, ": reward: ", reward, "\t(", reward*(n_comb+n_states), "/", (n_comb+n_states), ")")
+      log_config = clean_str_config(str_config) + '.log'
+      if i_episode==0:
+         output_f = open(log_config, "w")
+      else:
+         output_f = open(log_config, "a")
+      output_f.write("episode %s"   % repr(i_episode)                )
+      output_f.write(": reward: %s" % repr(reward)                   )
+      output_f.write("\t (%s"       % repr(reward*(n_comb+n_states)) )
+      output_f.write("/%s)\n"       % repr(n_comb+n_states)          )
+      output_f.closed
+
+def print_stats(dut, n_states, best_reward, best_episode_states):
+   stats_f = open("stats.log", "w")
+   stats_f.write("dut.states_covered = %s\n" % repr(dut.states_covered) )
+
+   for i in range(n_states):
+      for j in range(n_states):
+         if dut.comb_covered[i][j] != '-': 
+            stats_f.write("dut.comb_covered[%s"   % repr(i))
+            stats_f.write("][%s"   % repr(j))
+            stats_f.write("] = %s\t"   % repr(dut.comb_covered[i][j])   )
+      stats_f.write("\n")
+
+   # stats_f.write("dut.COMB = %s"             % repr(dut.COMB)           )
+   stats_f.closed
+
+   print("best reward = ", best_reward)
+   best_episode_f = open("best_epsiode.log", "w")
+   temp=[]
+   for i in range (len(best_episode_states)-1):
+      temp.append(best_episode_states[i])
+      if(best_episode_states[i+1] != best_episode_states[i]) or ((i+1) == len(best_episode_states)):
+         best_episode_f.write("%s" % repr(temp[0])   )
+         best_episode_f.write("("                    )
+         best_episode_f.write("%s" % repr(len(temp)) )
+         best_episode_f.write(")\t"                  )
+         temp=[]
+   best_episode_f.closed
+
---
+++ random_stimuli.py
@@ -0,0 +1,58 @@
+from Dut import Dut
+import matplotlib.pyplot as plt
+import numpy as np
+import pdb
+import print_stats
+
+CONFIGS = [ [False,   8,   32],
+            [ True,   8,   32],
+            [False,  32,  256],
+            [ True,  32,  256],
+            [False, 128, 1024],
+            [ True, 128, 1024] ]
+
+for config in CONFIGS:
+
+   print(config)
+   str_config = 'random_' + str(config)
+   DO_MERGE = config[0] 
+   N_INPUTS = config[1] 
+   N_STATES = config[2] 
+   
+   num_episodes = 1000
+   t_max        =  500
+   
+   rewards_l = []
+   
+   best_reward = -1
+   best_episode_states = []
+   episode_states = []
+   
+   
+   dut = Dut(N_STATES, N_INPUTS)
+   
+   for i_episode in range(num_episodes):
+       n_comb = dut.n_comb
+       state = dut.reset(DO_MERGE)
+       step = 1
+       while step <= t_max:
+           episode_states.append(state)
+           # apply random stimuli
+           action = int((2**N_INPUTS)*np.random.random())
+           next_state, reward, done = dut.step(state, action)
+           state = next_state
+           if done or (step == t_max):
+              print_stats.print_episode(i_episode, reward, n_comb, N_STATES, str_config)
+              rewards_l.append(reward)
+              if reward > best_reward:
+                 best_reward = reward
+                 best_episode_states = episode_states
+           step = step + 1
+   
+   print_stats.print_stats(dut, N_STATES, best_reward, best_episode_states)
+   # plt.plot(rewards_l)
+   png_config = 'rewards_' + print_stats.clean_str_config(str_config) + '.png'
+   plt.savefig(png_config)
+
+
+# stat on action generated per state
